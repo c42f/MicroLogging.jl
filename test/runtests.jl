cd56d53ff211e31c0aeb86b3a7a0748f8db2e3fa
Chris Foster <chris42f@gmail.com>
2017-03-19 10:48:46 +1000

    Tests for structured logging, more test cleanup
    * Test for key value pair structured logging.
    * Clean up the way the tests are expressed to improve clarity and reduce
      boilerplate.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,21 +3,46 @@ using Base.Test
 
 import MicroLogging: Debug, Info, Warn, Error
 
-type TestHandler
-    messages::Vector{Any}
+if VERSION < v"0.6-"
+    # Override Test.@test_broken, which is broken on julia-0.5!
+    # See https://github.com/JuliaLang/julia/issues/21008
+    macro test_broken(exs...)
+        esc(:(@test !($(exs...))))
+    end
 end
 
-TestHandler() = TestHandler(Vector{Any}())
+
+type LogRecord
+    context
+    level
+    message
+    kwargs
+end
+
+type TestHandler
+    records
+end
+
+TestHandler() = TestHandler(LogRecord[])
 
 function MicroLogging.logmsg(handler::TestHandler, context, level, msg; kwargs...)
-    push!(handler.messages, (context, level, msg))
+    push!(handler.records, LogRecord(context, level, msg, kwargs))
+end
+
+function records!(handler::TestHandler)
+    rs = handler.records
+    handler.records = LogRecord[]
+    rs
+end
+
+function simple_records!(handler::TestHandler)
+    [(r.context, r.level, r.message) for r in records!(handler)]
 end
 
 @testset "MicroLogging" begin
 
 #-------------------------------------------------------------------------------
 @testset "Basic logging" begin
-
     handler = TestHandler()
     configure_logging(handler=handler)
 
@@ -26,42 +51,47 @@ end
     @info  "b"
     @warn  "c"
     @error "d"
-    @test handler.messages == [(Main, Debug, "a"),
-                               (Main, Info , "b"),
-                               (Main, Warn , "c"),
-                               (Main, Error, "d")]
-    empty!(handler.messages)
+    @test simple_records!(handler) == [
+        (Main, Debug, "a"),
+        (Main, Info , "b"),
+        (Main, Warn , "c"),
+        (Main, Error, "d")
+    ]
 
     configure_logging(level=MicroLogging.Info)
     @debug "a"
     @info  "b"
     @warn  "c"
     @error "d"
-    @test handler.messages == [(Main, Info , "b"),
-                               (Main, Warn , "c"),
-                               (Main, Error, "d")]
-    empty!(handler.messages)
+    @test simple_records!(handler) == [
+        (Main, Info , "b"),
+        (Main, Warn , "c"),
+        (Main, Error, "d")
+    ]
 
     configure_logging(level=MicroLogging.Warn)
     @debug "a"
     @info  "b"
     @warn  "c"
     @error "d"
-    @test handler.messages == [(Main, Warn , "c"),
-                               (Main, Error, "d")]
-    empty!(handler.messages)
+    @test simple_records!(handler) == [
+        (Main, Warn , "c"),
+        (Main, Error, "d")
+    ]
 
     configure_logging(level=MicroLogging.Error)
     @debug "a"
     @info  "b"
     @warn  "c"
     @error "d"
-    @test handler.messages == [(Main, Error, "d")]
-    empty!(handler.messages)
-
+    @test simple_records!(handler) == [
+        (Main, Error, "d")
+    ]
 end
 
+
 #-------------------------------------------------------------------------------
+# Macro front end
 
 @testset "Log to custom logger" begin
     handler = TestHandler()
@@ -72,10 +102,12 @@ end
     @warn  logger "c"
     @error logger "d"
 
-    @test handler.messages == [(:TestContext, Debug, "a"),
-                               (:TestContext, Info , "b"),
-                               (:TestContext, Warn , "c"),
-                               (:TestContext, Error, "d")]
+    @test simple_records!(handler) == [
+        (:TestContext, Debug, "a"),
+        (:TestContext, Info , "b"),
+        (:TestContext, Warn , "c"),
+        (:TestContext, Error, "d")
+    ]
 end
 
 
@@ -92,9 +124,33 @@ end
     @info logger "$i"
     @info logger @sprintf("%.3f", i)
 
-    @test handler.messages == [(:TestContext, Info, "sum(A) = 16.0"),
-                               (:TestContext, Info, "10.5"),
-                               (:TestContext, Info, "10.500")]
+    @test simple_records!(handler) == [
+        (:TestContext, Info, "sum(A) = 16.0"),
+        (:TestContext, Info, "10.5"),
+        (:TestContext, Info, "10.500")
+    ]
+end
+
+@testset "Structured logging with key value pairs" begin
+    handler = TestHandler()
+    logger = Logger(:TestContext, MicroLogging.Info, handler)
+
+    foo_val = 10
+    expected_log_line = 1 + @__LINE__
+    @info logger "test" progress=0.1 foo=foo_val
+    recs = records!(handler)
+
+    @test length(recs) == 1
+    kwargs = Dict(recs[1].kwargs)
+
+    # Builtin metadata
+    @test kwargs[:location][1] == Base.source_path()
+    @test_broken kwargs[:location][2] == expected_log_line # See #1
+    @test kwargs[:module_] == Main
+
+    # User-defined metadata
+    @test kwargs[:progress] == 0.1
+    @test kwargs[:foo] == foo_val
 end
 
 
@@ -137,91 +193,33 @@ end
 @testset "Logger heirarchy" begin
     handler = TestHandler()
     B_handler = TestHandler()
+    # Install root handler
     configure_logging(handler=handler)
     configure_logging(A, level=Info)
+    # Override root handler for module B and its children
     configure_logging(A.B, level=Warn, handler=B_handler)
     configure_logging(A.B.C, level=Error)
+
     A.a()
-    @test handler.messages == [(A, Info , "a"),
-                               (A, Warn , "a"),
-                               (A, Error, "a")]
-    empty!(handler.messages)
+    @test simple_records!(handler) == [
+        (A, Info , "a"),
+        (A, Warn , "a"),
+        (A, Error, "a")
+    ]
 
     A.B.b()
-    @test B_handler.messages == [(A.B, Warn , "b"),
-                                 (A.B, Error, "b")]
-    @test isempty(handler.messages)
-    empty!(B_handler.messages)
+    @test isempty(records!(handler))
+    @test simple_records!(B_handler) == [
+        (A.B, Warn , "b"),
+        (A.B, Error, "b")
+    ]
 
     A.B.C.c()
-    @test B_handler.messages == [(A.B.C, Error, "c")]
-    @test isempty(handler.messages)
+    @test isempty(records!(handler))
+    @test simple_records!(B_handler) == [
+        (A.B.C, Error, "c")
+    ]
 end
 
-
-#-------------------------------------------------------------------------------
-#=
-
-function logger_macro_forms()
-    # Access logger
-    @info logger "bar()"
-    # Computation
-    @info begin
-        a = 0.5
-        @sprintf("a = %.2f, a^2 = %.2f", a, a.^2)
-    end
-end
-
-
-function baz()
-    # Loggers as variables
-    handler = MicroLogging.LogHandler(STDOUT)
-    logger = Logger(:LocalLogger, handler, MicroLogging.Warn)
-    @debug logger "baz()"
-    @info  logger "baz()"
-    @warn  logger "baz()"
-    @error logger "baz()"
-end
-
-
-#=
-function qux()
-    # Custom log levels ??
-    @logmsg logger :mylevel "asdf" 
-    @logmsg Debug2 "asdf" 
-    @logmsg :level "asdf" 
-end
-=#
-
-
-type TestHandler
-    messages::Vector{Any}
-end
-
-TestHandler() = TestHandler(Vector{Any}())
-
-MicroLogging.logmsg(handler::TestHandler, loggername, level, location, msg) =
-    push!(handler.messages, (loggername, level, msg))
-
-
-@testset "Basic logging"
-
-    handler = TestHandler()
-    configure_logging(Main,
-configure_logging(Main,
-    level=MicroLogging.Debug,
-    handler=TestHandler()
-)
-Foo.foo(1)
-Foo.foo(1)
-
-println()
-
-Foo.Bar.bar()
-
-println()
-Foo.Bar.baz()
-
-=#
 
 end
