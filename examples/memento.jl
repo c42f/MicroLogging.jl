32e07c733f9d890a37740b0904cc974bd4a36914
Chris Foster <chris42f@gmail.com>
2017-05-14 12:21:38 +1000

    WIP - case study of plugging the hierarcy-based Memento.jl as a backend

---
+++ examples/memento.jl
@@ -0,0 +1,74 @@
+# An simple example of how Memento can be plugged in as a backend to MicroLogging
+
+# ------------------------------------------------------------------------------
+module MementoShim
+
+using MicroLogging
+using Memento
+
+# Confusingly, this is the thing which goes by the name "logger" in
+# MicroLogging terminology.  Here it's just used as a placeholder to dispatch
+# to the global Memento logger hierarchy.
+immutable MementoBackend
+end
+
+function MicroLogging.logmsg(::MementoBackend, ml_level, msg, module_, filepath, line, id; kwargs...)
+    # TODO: Map the `group` keyword (or something equivalent) into the right logger.
+    # For now, assume a module-based logger.
+    logger = get_logger(module_)
+
+    if     ml_level == MicroLogging.Debug ; level = "debug"
+    elseif ml_level == MicroLogging.Info  ; level = "info"
+    elseif ml_level == MicroLogging.Warn  ; level = "warn"
+    elseif ml_level == MicroLogging.Error ; level = "error"
+    end
+
+    # TODO: Capture all the keyword arguments, and other call site metadata
+    rec = logger.record(logger.name, level, logger.levels[level], msg)
+    @sync log(logger, rec)
+end
+
+function MicroLogging.shouldlog(::MementoBackend, ml_level, module_, filepath, line, id, max_log, progress)
+    # TODO - implement early out filtering for efficiency
+    true
+end
+
+end
+
+
+# ------------------------------------------------------------------------------
+# A test module containing some logging
+module A
+
+using MicroLogging
+
+function foo()
+    @debug "message from micrologging"
+    @info  "message from micrologging"
+    @warn  "message from micrologging"
+    @error "message from micrologging"
+end
+
+end
+
+
+# ------------------------------------------------------------------------------
+# Log configuration
+using MicroLogging
+using Memento
+
+# MicroLogging setup - set Memento as backend
+global_logger(MementoShim.MementoBackend())
+
+# Configure Memento
+Memento.config("info")
+# Do something special for messages coming from module A
+A_logger = get_logger(A)
+add_handler(A_logger, DefaultHandler(Syslog(:local0, "julia"), DefaultFormatter("{level} {name}: {msg}")))
+set_level(A_logger, "warn")
+
+
+# Now, do the actaul logging
+@info "Global log message"
+A.foo()
+
